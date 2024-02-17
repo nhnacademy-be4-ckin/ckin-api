dad2a5bf974baa750962517484f039356c840a4a
dduneon <dduneon@gmail.com>
2024-02-17 17:47:12 +0900

    [ADD] Exception 처리를 위한 커스텀 Exception 생성

---
+++ src/main/java/store/ckin/api/tag/exception/TagNameAlreadyExistException.java
@@ -0,0 +1,13 @@
+package store.ckin.api.tag.exception;
+
+/**
+ * description
+ *
+ * @author 김준현
+ * @version 2024. 02. 17
+ */
+public class TagNameAlreadyExistException extends RuntimeException{
+    public TagNameAlreadyExistException(String tagName) {
+        super(String.format("태그(name: %s)는 이미 존재하는 태그 이름입니다", tagName));
+    }
+}
