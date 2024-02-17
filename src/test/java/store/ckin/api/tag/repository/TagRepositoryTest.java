5aec7d64b4255a29babc1c84c7ee26da790847d4
dduneon <dduneon@gmail.com>
2024-02-17 19:09:38 +0900

    [TEST] 태그 Repository 테스트 메서드 추가

--- src/test/java/store/ckin/api/tag/repository/TagRepositoryTest.java
+++ src/test/java/store/ckin/api/tag/repository/TagRepositoryTest.java
@@ -2,7 +2,15 @@ package store.ckin.api.tag.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Transactional;
+import store.ckin.api.tag.dto.response.TagResponseDto;
+import store.ckin.api.tag.entity.Tag;
 
 /**
  * description
@@ -10,9 +18,38 @@ import org.junit.jupiter.api.Test;
  * @author 김준현
  * @version 2024. 02. 17
  */
+
+@DataJpaTest
+@Transactional
 class TagRepositoryTest {
+    @Autowired
+    private TagRepository tagRepository;
+
+    @BeforeEach
+    void setUp() {
+        Tag testTag1 = Tag.builder()
+                .tagName("테스트 태그1")
+                .build();
+        Tag testTag2 = Tag.builder()
+                .tagName("테스트 태그2")
+                .build();
+
+        tagRepository.save(testTag1);
+        tagRepository.save(testTag2);
+    }
 
     @Test
-    void findAllTags() {
+    @DisplayName("태그 전체 조회")
+    void findAllTagsTest() {
+        List<TagResponseDto> actual = tagRepository.findAllTags();
+        assertEquals(2, actual.size());
+    }
+
+    @Test
+    @DisplayName("태그 이름 존재 여부 테스트")
+    void existByTagNameTest() {
+        assertTrue(tagRepository.existsByTagName("테스트 태그1"));
+        assertTrue(tagRepository.existsByTagName("테스트 태그2"));
+        assertFalse(tagRepository.existsByTagName("존재하지 않는 태그"));
     }
 }
