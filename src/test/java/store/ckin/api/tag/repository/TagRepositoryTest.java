4857912d7bb0302d94976523196ddeb17ad97db5
dduneon <dduneon@gmail.com>
2024-02-17 17:21:40 +0900

    [TEST] 태그 테스트 클래스 생성

---
+++ src/test/java/store/ckin/api/tag/repository/TagRepositoryTest.java
@@ -0,0 +1,18 @@
+package store.ckin.api.tag.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * description
+ *
+ * @author 김준현
+ * @version 2024. 02. 17
+ */
+class TagRepositoryTest {
+
+    @Test
+    void findAllTags() {
+    }
+}
---
+++ src/test/java/store/ckin/api/tag/service/impl/TagServiceImplTest.java
@@ -0,0 +1,30 @@
+package store.ckin.api.tag.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * description
+ *
+ * @author 김준현
+ * @version 2024. 02. 17
+ */
+class TagServiceImplTest {
+
+    @Test
+    void readTagList() {
+    }
+
+    @Test
+    void createTag() {
+    }
+
+    @Test
+    void updateTag() {
+    }
+
+    @Test
+    void deleteTag() {
+    }
+}
