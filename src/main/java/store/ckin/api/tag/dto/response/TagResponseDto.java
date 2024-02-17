aa567a9461c78084082d57798daaca98a5c6eb7a
dduneon <dduneon@gmail.com>
2024-02-17 19:08:04 +0900

    [REMOVE] AllArgsConstructor 어노테이션 제거

--- src/main/java/store/ckin/api/tag/dto/response/TagResponseDto.java
+++ src/main/java/store/ckin/api/tag/dto/response/TagResponseDto.java
@@ -1,6 +1,5 @@
 package store.ckin.api.tag.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
@@ -9,7 +8,6 @@ import lombok.Getter;
  * @author 김준현
  * @version 2024. 02. 16
  */
-@AllArgsConstructor
 @Getter
 public class TagResponseDto {
     private Long tagId;
