f73aed1303a3e5abd55e74985f002337f12505e9
f1v3-dev <f1v3@kakao.com>
2024-02-18 17:44:12 +0900

    [FEAT] DBCP2 추가

--- src/main/java/store/ckin/api/ApiApplication.java
+++ src/main/java/store/ckin/api/ApiApplication.java
@@ -2,10 +2,12 @@ package store.ckin.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ApiApplication {
 
     public static void main(String[] args) {
