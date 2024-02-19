4174d04e9fce8e83e26c6d1d083fb8f854c054a5
dduneon <dduneon@gmail.com>
2024-02-19 18:21:26 +0900

    [UPDATE] DataSource 로드 시 KeyManager 를 통하도록 로직 변경

--- src/main/java/store/ckin/api/config/DataSourceConfig.java
+++ src/main/java/store/ckin/api/config/DataSourceConfig.java
@@ -2,9 +2,11 @@ package store.ckin.api.config;
 
 import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import store.ckin.api.skm.util.KeyManager;
 
 /**
  * DBCP2 설정을 위한 클래스입니다.
@@ -13,20 +15,22 @@ import org.springframework.context.annotation.Configuration;
  * @version 2024. 02. 16.
  */
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class DataSourceConfig {
 
     private final DbProperties dbProperties;
+    private final KeyManager keyManager;
 
     @Bean
     public DataSource dataSource() {
         BasicDataSource basicDataSource = new BasicDataSource();
 
-        basicDataSource.setDriverClassName(dbProperties.getDriver());
-        basicDataSource.setUrl(dbProperties.getUrl());
-        basicDataSource.setUsername(dbProperties.getUsername());
-        basicDataSource.setPassword(dbProperties.getPassword());
+        basicDataSource.setDriverClassName(keyManager.keyStore(dbProperties.getDriver()));
+        basicDataSource.setUrl(keyManager.keyStore(dbProperties.getUrl()));
+        basicDataSource.setUsername(keyManager.keyStore(dbProperties.getUsername()));
+        basicDataSource.setPassword(keyManager.keyStore(dbProperties.getPassword()));
 
         basicDataSource.setInitialSize(dbProperties.getInitialSize());
         basicDataSource.setMaxTotal(dbProperties.getMaxTotal());
