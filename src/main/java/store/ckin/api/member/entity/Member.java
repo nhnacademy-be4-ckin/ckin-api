0365acccc33e761b372d33913f70fe0fd38558a7
jinwoo Lee <jinwoolee@jinwooui-noteubug.local>
2024-02-20 01:39:31 +0900

    [REFACTOR] import 정리

--- src/main/java/store/ckin/api/member/entity/Member.java
+++ src/main/java/store/ckin/api/member/entity/Member.java
@@ -1,14 +1,19 @@
 package store.ckin.api.member.entity;
 
 import java.time.LocalDateTime;
-import javax.persistence.*;
-
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
 import store.ckin.api.grade.entity.Grade;
 
