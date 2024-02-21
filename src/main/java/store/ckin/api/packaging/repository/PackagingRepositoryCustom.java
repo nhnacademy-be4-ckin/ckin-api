5343ebb08aae8a312f0da62049800263634cdbcd
f1v3-dev <f1v3@kakao.com>
2024-02-21 15:37:05 +0900

    [REFACTOR] @NoRepositoryBean 추가

--- src/main/java/store/ckin/api/packaging/repository/PackagingRepositoryCustom.java
+++ src/main/java/store/ckin/api/packaging/repository/PackagingRepositoryCustom.java
@@ -1,11 +1,14 @@
 package store.ckin.api.packaging.repository;
 
+import org.springframework.data.repository.NoRepositoryBean;
+
 /**
  * 포장 정책 Repository Querydsl 사용할 메서드가 있는 인터페이스입니다.
  *
  * @author 정승조
  * @version 2024. 02. 21.
  */
+@NoRepositoryBean
 public interface PackagingRepositoryCustom {
 
     boolean existsByType(String type);
