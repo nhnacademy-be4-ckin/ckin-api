3ccbdf775da5f8bac80c026c0de47f9fa66f9e6d
nayoseb <nayoseb1018@gmail.com>
2024-02-26 21:54:17 +0900

    [FEAT] BookAuthor CRUD 구현

---
+++ src/main/java/store/ckin/api/relationship/bookauthor/repository/BookAuthorRepository.java
@@ -0,0 +1,8 @@
+package store.ckin.api.relationship.bookauthor.repository;/**
+ * {class name}.
+ * 
+ * @author 나국로
+ * @version 2024. 02. 26.
+*/
+public interface BookAuthorRepository {
+}
---
+++ src/main/java/store/ckin/api/relationship/bookauthor/repository/impl/BookAuthorRepositoryImpl.java
@@ -0,0 +1,8 @@
+package store.ckin.api.relationship.bookauthor.repository.impl;/**
+ * {class name}.
+ * 
+ * @author 나국로
+ * @version 2024. 02. 26.
+*/
+public class BookAuthorRepositoryImpl {
+}
