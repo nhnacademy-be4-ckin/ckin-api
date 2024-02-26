d3f8b5d4ccb308e1aae023a74871c89dc7a35e27
nayoseb <nayoseb1018@gmail.com>
2024-02-26 22:01:33 +0900

    [FEAT] BookNotFoundException 추가

---
+++ src/main/java/store/ckin/api/book/exception/BookNotFoundException.java
@@ -0,0 +1,14 @@
+package store.ckin.api.book.exception;
+
+/**
+ * BookNotFoundException 예외클래스.
+ *
+ * @author 나국로
+ * @version 2024. 02. 26.
+ */
+public class BookNotFoundException extends RuntimeException {
+    public BookNotFoundException(Long bookId) {
+        super(String.format("책(id: %d)을 찾을 수 없습니다", bookId));
+
+    }
+}
