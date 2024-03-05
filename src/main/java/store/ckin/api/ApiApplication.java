c1f05628b7cdc379b15b1553f818e73b6dafcb21
nayoseb <nayoseb1018@gmail.com>
2024-03-05 09:39:30 +0900

    [FEATURE] TokenDto 생성 및 Auditable로 생성시간 추가

--- src/main/java/store/ckin/api/ApiApplication.java
+++ src/main/java/store/ckin/api/ApiApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableEurekaClient
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableJpaAuditing
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
---
+++ src/main/java/store/ckin/api/objectstorage/dto/response/TokenResponse.java
@@ -0,0 +1,29 @@
+package store.ckin.api.objectstorage.dto.response;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * {class name}.
+ *
+ * @author 나국로
+ * @version 2024. 03. 04.
+ */
+@Getter
+public class TokenResponse {
+    private Access access;
+
+    @Getter
+    @NoArgsConstructor
+    public static class Access {
+        Token token;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class Token {
+        LocalDateTime expires;
+        String id;
+    }
+}
