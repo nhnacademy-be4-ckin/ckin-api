f898014a0cbe27a38d07282cecb2c41b86a4ac35
f1v3-dev <f1v3@kakao.com>
2024-03-16 23:52:26 +0900

    [TEST] 테스트 코드 추

--- src/test/java/store/ckin/api/payment/facade/PaymentFacadeTest.java
+++ src/test/java/store/ckin/api/payment/facade/PaymentFacadeTest.java
@@ -1,6 +1,7 @@
 package store.ckin.api.payment.facade;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.anyString;
@@ -17,6 +18,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+import store.ckin.api.member.service.MemberService;
 import store.ckin.api.payment.dto.request.PaymentRequestDto;
 import store.ckin.api.payment.exception.PaymentAmountNotCorrectException;
 import store.ckin.api.payment.exception.PaymentNotCompleteException;
@@ -44,6 +47,9 @@ class PaymentFacadeTest {
     @Mock
     SaleService saleService;
 
+    @Mock
+    MemberService memberService;
+
     PaymentRequestDto failPayment;
 
     PaymentRequestDto successPayment;
@@ -51,26 +57,23 @@ class PaymentFacadeTest {
 
     @BeforeEach
     void setUp() {
-        failPayment = new PaymentRequestDto(
-                "12341234",
-                "423421432",
-                "FAIL",
-                LocalDateTime.now().minusMinutes(10),
-                LocalDateTime.now(),
-                15000,
-                "https://test.com"
-        );
+        failPayment = new PaymentRequestDto();
+        ReflectionTestUtils.setField(failPayment, "paymentKey", "12341234");
+        ReflectionTestUtils.setField(failPayment, "saleNumber", "423421432");
+        ReflectionTestUtils.setField(failPayment, "paymentStatus", "FAIL");
+        ReflectionTestUtils.setField(failPayment, "requestedAt", LocalDateTime.now().minusMinutes(10));
+        ReflectionTestUtils.setField(failPayment, "approvedAt", LocalDateTime.now());
+        ReflectionTestUtils.setField(failPayment, "amount", 15000);
+        ReflectionTestUtils.setField(failPayment, "receiptUrl", "https://test.com");
 
-        successPayment = new PaymentRequestDto(
-                "12341234",
-                "423421432",
-                "DONE",
-                LocalDateTime.now().minusMinutes(10),
-                LocalDateTime.now(),
-                15000,
-                "https://test.com"
-        );
-
+        successPayment = new PaymentRequestDto();
+        ReflectionTestUtils.setField(successPayment, "paymentKey", "12341234");
+        ReflectionTestUtils.setField(successPayment, "saleNumber", "423421432");
+        ReflectionTestUtils.setField(successPayment, "paymentStatus", "DONE");
+        ReflectionTestUtils.setField(successPayment, "requestedAt", LocalDateTime.now().minusMinutes(10));
+        ReflectionTestUtils.setField(successPayment, "approvedAt", LocalDateTime.now());
+        ReflectionTestUtils.setField(successPayment, "amount", 15000);
+        ReflectionTestUtils.setField(successPayment, "receiptUrl", "https://test.com");
 
     }
 
@@ -93,6 +96,7 @@ class PaymentFacadeTest {
 
         SaleResponseDto saleResponseDto =
                 new SaleResponseDto(
+                        1L,
                         1L,
                         "테스트 제목",
                         "test@test.com",
@@ -132,6 +136,7 @@ class PaymentFacadeTest {
         SaleResponseDto saleResponseDto =
                 new SaleResponseDto(
                         1L,
+                        1L,
                         "테스트 제목",
                         "test@test.com",
                         "1234",
@@ -158,5 +163,76 @@ class PaymentFacadeTest {
 
         verify(saleService, times(1)).getSaleBySaleNumber(anyString());
         verify(paymentService, times(1)).createPayment(anyLong(), any());
+    }
+
+    @Test
+    @DisplayName("포인트 적립 테스트 - 회원일 경우")
+    void testCreateRewardPoint_Member() {
+        SaleResponseDto saleResponseDto =
+                new SaleResponseDto(
+                        1L,
+                        1L,
+                        "테스트 제목",
+                        "test@test.com",
+                        "1234",
+                        "정승조",
+                        "01012345678",
+                        "정승조",
+                        "01012345678",
+                        "광주광역시 동구 조선대 5길",
+                        LocalDateTime.now(),
+                        LocalDateTime.now().plusDays(1),
+                        LocalDate.now().plusDays(3),
+                        Sale.DeliveryStatus.READY,
+                        3000,
+                        0,
+                        15000,
+                        Sale.PaymentStatus.WAITING,
+                        "123456"
+                );
+
+        given(saleService.getSaleBySaleNumber(anyString()))
+                .willReturn(saleResponseDto);
+
+        paymentFacade.createRewardPoint("123456");
+
+        verify(saleService, times(1)).getSaleBySaleNumber(anyString());
+        verify(memberService, times(1)).updateRewardPoint(anyString(), anyInt());
+    }
+
+    @Test
+    @DisplayName("포인트 적립 테스트 - 비회원일 경우")
+    void testCreateRewardPoint_Anonymous() {
+        SaleResponseDto saleResponseDto =
+                new SaleResponseDto(
+                        1L,
+                        1L,
+                        "테스트 제목",
+                        null,
+                        "123456",
+                        "정승조",
+                        "01012345678",
+                        "정승조",
+                        "01012345678",
+                        "광주광역시 동구 조선대 5길",
+                        LocalDateTime.now(),
+                        LocalDateTime.now().plusDays(1),
+                        LocalDate.now().plusDays(3),
+                        Sale.DeliveryStatus.READY,
+                        3000,
+                        0,
+                        15000,
+                        Sale.PaymentStatus.WAITING,
+                        "123456"
+                );
+
+
+        given(saleService.getSaleBySaleNumber(anyString()))
+                .willReturn(saleResponseDto);
+
+        paymentFacade.createRewardPoint("123456");
+
+        verify(saleService, times(1)).getSaleBySaleNumber(anyString());
+        verify(memberService, times(0)).updateRewardPoint(anyString(), anyInt());
     }
 }
