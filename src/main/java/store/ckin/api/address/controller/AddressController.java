80b240a97cce08f9c5e32d66f196497f5099fbfe
jinwoo Lee <jinwoolee@jinwooui-noteubug.local>
2024-03-18 01:31:00 +0900

    [FEAT] AddressController : addAddress

--- src/main/java/store/ckin/api/address/controller/AddressController.java
+++ src/main/java/store/ckin/api/address/controller/AddressController.java
@@ -1,8 +1,13 @@
 package store.ckin.api.address.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import store.ckin.api.address.domain.request.AddressAddRequestDto;
 import store.ckin.api.address.service.AddressService;
 
 /**
@@ -16,4 +21,12 @@ import store.ckin.api.address.service.AddressService;
 @RequiredArgsConstructor
 public class AddressController {
     private final AddressService addressService;
+
+    @PostMapping("/members/{memberId}/address")
+    ResponseEntity<Void> addAddress(@PathVariable("memberId") Long memberId,
+                                    AddressAddRequestDto addressAddRequestDto) {
+        addressService.addAddress(memberId, addressAddRequestDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
