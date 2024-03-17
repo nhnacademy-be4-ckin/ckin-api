2d09904fbd835588e5d395dcf7cb6973fe6e032c
jinwoo Lee <jinwoolee@jinwooui-noteubug.local>
2024-03-18 01:39:09 +0900

    [FEAT] setDefaultAddress

--- src/main/java/store/ckin/api/address/controller/AddressController.java
+++ src/main/java/store/ckin/api/address/controller/AddressController.java
@@ -4,11 +4,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import store.ckin.api.address.domain.request.AddressAddRequestDto;
 import store.ckin.api.address.domain.request.AddressUpdateRequestDto;
 import store.ckin.api.address.domain.response.MemberAddressResponseDto;
@@ -41,11 +37,19 @@ public class AddressController {
         return ResponseEntity.status(HttpStatus.OK).body(addressList);
     }
 
-    @PostMapping("/members/{memberId}/addresses/{addressId}")
+    @PutMapping("/members/{memberId}/addresses/{addressId}")
     ResponseEntity<Void> updateAddress(@PathVariable("memberId") Long memberId,
                                        @PathVariable("addressId") Long addressId,
                                        AddressUpdateRequestDto addressUpdateRequestDto) {
         addressService.updateAddress(memberId, addressId, addressUpdateRequestDto);
+
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @PutMapping("/members/{memberId}/addresses/{addressId}/default")
+    ResponseEntity<Void> setDefaultAddress(@PathVariable("memberId") Long memberId,
+                                       @PathVariable("addressId") Long addressId) {
+        addressService.setDefaultAddress(memberId, addressId);
 
         return ResponseEntity.status(HttpStatus.OK).build();
     }
