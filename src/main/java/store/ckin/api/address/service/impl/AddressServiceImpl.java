377ad283d797f53716b1da58011039a308f1be90
jinwoo Lee <jinwoolee@jinwooui-noteubug.local>
2024-03-18 00:23:59 +0900

    [FEAT] AddressServiceImpl

---
+++ src/main/java/store/ckin/api/address/service/impl/AddressServiceImpl.java
@@ -0,0 +1,52 @@
+package store.ckin.api.address.service.impl;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import store.ckin.api.address.domain.request.AddressAddRequestDto;
+import store.ckin.api.address.domain.request.AddressUpdateRequestDto;
+import store.ckin.api.address.domain.response.MemberAddressResponseDto;
+import store.ckin.api.address.repository.AddressRepository;
+import store.ckin.api.address.service.AddressService;
+
+/**
+ * AddressService 의 구현체 입니다.
+ *
+ * @author : jinwoolee
+ * @version : 2024. 03. 18.
+ */
+@Service
+@RequiredArgsConstructor
+public class AddressServiceImpl implements AddressService {
+    private final AddressRepository addressRepository;
+
+    @Override
+    public boolean isDefaultAddress(Long addressId) {
+        return false;
+    }
+
+    @Override
+    public void addAddress(Long memberId, AddressAddRequestDto addressAddRequestDto) {
+
+    }
+
+    @Override
+    public List<MemberAddressResponseDto> getMemberAddressList(Long memberId) {
+        return null;
+    }
+
+    @Override
+    public void updateAddress(Long memberId, AddressUpdateRequestDto addressUpdateRequestDto) {
+
+    }
+
+    @Override
+    public void setDefaultAddress(Long addressId) {
+
+    }
+
+    @Override
+    public void deleteAddress(Long addressId) {
+
+    }
+}
