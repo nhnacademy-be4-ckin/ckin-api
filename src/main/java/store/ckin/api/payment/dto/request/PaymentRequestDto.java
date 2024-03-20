c7023e059322b90966fa5df64857cb19f45991ca
f1v3-dev <f1v3@kakao.com>
2024-03-20 16:22:45 +0900

    [TEST] 결제 RestDocs

--- src/main/java/store/ckin/api/payment/dto/request/PaymentRequestDto.java
+++ src/main/java/store/ckin/api/payment/dto/request/PaymentRequestDto.java
@@ -1,6 +1,8 @@
 package store.ckin.api.payment.dto.request;
 
 import java.time.LocalDateTime;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import store.ckin.api.payment.entity.PaymentStatus;
@@ -16,17 +18,23 @@ import store.ckin.api.payment.entity.PaymentStatus;
 @NoArgsConstructor
 public class PaymentRequestDto {
 
+    @NotBlank(message = "결제 키는 필수입니다.")
     private String paymentKey;
 
+    @NotBlank(message = "결제 번호는 필수입니다.")
     private String saleNumber;
 
+    @NotNull(message = "결제 상태는 필수입니다.")
     private PaymentStatus paymentStatus;
 
+    @NotNull(message = "결제 요청 시간은 필수입니다.")
     private LocalDateTime requestedAt;
 
     private LocalDateTime approvedAt;
 
+    @NotNull(message = "결제 금액은 필수입니다.")
     private Integer amount;
 
+    @NotBlank(message = "영수증 URL은 필수입니다.")
     private String receiptUrl;
 }
