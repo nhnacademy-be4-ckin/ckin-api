e8ebcd7e48fe16bd4eecbf45f6075a9bcdae67d3
jinwoo Lee <jinwoolee@jinwooui-noteubug.local>
2024-03-21 15:37:10 +0900

    [REFACTOR] updateGrade method 변경

--- src/main/java/store/ckin/api/grade/controller/GradeController.java
+++ src/main/java/store/ckin/api/grade/controller/GradeController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -57,7 +56,7 @@ public class GradeController {
      *
      * @return OK (Code 200)
      */
-    @PutMapping("/{gradeId}")
+    @PostMapping("/{gradeId}")
     public ResponseEntity<Void> updateGrade(@PathVariable("gradeId") Long gradeId,
                                             @Valid @RequestBody GradeUpdateRequestDto gradeUpdateRequestDto) {
         gradeService.updateGrade(gradeId, gradeUpdateRequestDto);
