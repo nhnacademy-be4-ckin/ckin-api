2ab95a39839100d434bb71806b3fda91ebc336a5
dduneon <dduneon@gmail.com>
2024-03-26 19:25:11 +0900

    [ADD] 카트가 존재하지 않을 때 발생하는 Exception 추가

---
+++ src/main/java/store/ckin/api/cart/dto/CartCreateRequestDto.java
@@ -0,0 +1,21 @@
+package store.ckin.api.cart.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 카트 생성 요청을 위한 Dto 클래스
+ *
+ * @author : dduneon
+ * @version : 2024. 03. 26
+ */
+@Getter
+@NoArgsConstructor
+public class CartCreateRequestDto {
+    @NotNull
+    private Long userId;
+    @NotBlank
+    private String cartId;
+}
